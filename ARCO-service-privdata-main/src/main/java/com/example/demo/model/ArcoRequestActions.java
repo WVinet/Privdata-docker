8b6cd04f1fa0e344ab88f37ad435e0bf2fbd122f
WVinet <w.vinet.h@gmail.com>
2026-05-20 12:08:13 -0400

    Se agrega Arco-Service

---
+++ ARCO-service-privdata-main/src/main/java/com/example/demo/model/ArcoRequestActions.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+import com.example.demo.enums.arcoRequestAction.ArcoActionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArcoRequestActions {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+//    @Column(name = "arco_request_id", nullable = false)
+//    private UUID arcoRequestId;
+    @Column(name = "executed_by_user_id", nullable = false)
+    private UUID executedByUserId;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "action_type", nullable = false)
+    private ArcoActionType actionType;
+    @Column(name = "result_summary", nullable = false)
+    private String resultSummary;
+    @Column(name = "artifact_url", nullable = false)
+    private String artifactUrl;
+    @CreationTimestamp
+    @Column(name = "executed_at", nullable = false)
+    private LocalDateTime executedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "arco_request_id", nullable = false)
+    private ArcoRequest arcoRequest;
+}
---
+++ ARCO-service-privdata-main/src/main/java/com/example/demo/model/ArcoRequestStatusHistory.java
@@ -0,0 +1,48 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.example.demo.enums.arcoRequestStatusHistory.ArcoHistoryStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArcoRequestStatusHistory {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+//    @Column(name = "arco_request_id", nullable = false)
+//    private UUID arcoRequestId;
+
+    @Column(name = "change_by_user_id", nullable = false)
+    private UUID changeByUserId;
+
+    //ambos enums usan el mismo enum
+    @Column(name="previous_status",nullable= false) //enum
+    private ArcoHistoryStatus previousStatus;
+
+    @Column(name="new_status",nullable= false) //enum
+    private ArcoHistoryStatus newStatus;
+
+    @UpdateTimestamp
+    @Column(name="change_at",nullable= false)
+    private LocalDateTime changeAt;
+
+    @Column(name="comment",nullable= false)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "arco_request_id", nullable = false)
+    private ArcoRequest arcoRequest;
+}
